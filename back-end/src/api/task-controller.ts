64c16a319d321676e54887cffcbf06385d895d38
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-06-22 10:27:36 +0530

    Expose some web services to  create task, update task, delete task and get all tasks

---
+++ back-end/src/api/task-controller.ts
@@ -0,0 +1,70 @@
+import express, {json} from "express";
+import mysql, {Pool} from "promise-mysql";
+
+export const router = express.Router();
+
+let pool:Pool;
+initPool();
+
+async function initPool(){
+    pool =  await mysql.createPool({
+        host: 'localhost',
+        port: 3306,
+        database: 'dep10_simple_task_app',
+        user: 'root',
+        password: 'mysql',
+        connectionLimit: 5,
+    });
+}
+
+type Task = {
+    id: number,
+    description: string,
+    status: 'COMPLETED' | 'NOT_COMPLETED' | undefined
+}
+
+/* Get all tasks */
+router.get("/", async (req, res)=>{
+    const tasks = await pool.query("SELECT *  FROM task");
+    res.json(tasks);
+});
+
+
+/* Create a new task*/
+router.post("/", async (req, res)=>{
+    const task = (req.body as Task);
+    if(!task.description?.trim()) {
+        res.sendStatus(400);
+        return;
+    }
+
+    const result = await pool.query("INSERT INTO task (description, status) VALUES (?, DEFAULT)",
+        [task.description]);
+
+    task.id = result.insertId;
+    task.status = "NOT_COMPLETED";
+
+    res.status(201).json(task);
+
+});
+
+/* Delete an existing task*/
+router.delete("/:taskId", async (req, res)=>{
+    const result = await pool.query("DELETE FROM task WHERE id=?",
+        [req.params.taskId]);
+    res.sendStatus(result.affectedRows ? 204 : 404);
+});
+
+
+/* Update an existing task*/
+router.patch("/:taskId", async (req, res)=>{
+    const task = (req.body as Task);
+    task.id = +req.params.taskId;
+    if(!task.status){
+        res.sendStatus(400);
+        return
+    }
+    const result = await pool.query("UPDATE task set status=? WHERE id=?",
+        [task.status, req.params.taskId]);
+    res.sendStatus(result.affectedRows ? 204 : 404);
+});
